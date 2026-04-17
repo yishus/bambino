d37a3213861f64a969f295247136cc52ddc3aa68
Yishu See <yishuyishu@gmail.com>
2026-04-18 00:51:27 +0800

    First commit

---
+++ bambino/tokenizer.py
@@ -0,0 +1,8 @@
+class BPETokenizer:
+    def __init__(
+        self,
+        vocab: dict[int, bytes],
+        merges: list[tuple[bytes, bytes]],
+        special_tokens: list[str] | None = None,
+    ) -> None:
+        pass
